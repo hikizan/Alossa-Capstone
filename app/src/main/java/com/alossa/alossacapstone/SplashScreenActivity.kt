4e30839459d8d2db7142c13a296caf529374a21d
Permana <permana4131@gmail.com>
2021-12-05 16:25:21 +0700

    Auth UI

--- app/src/main/java/com/alossa/alossacapstone/SplashScreenActivity.kt
+++ app/src/main/java/com/alossa/alossacapstone/SplashScreenActivity.kt
@@ -1,40 +1,21 @@
 package com.alossa.alossacapstone
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.alossa.alossacapstone.databinding.ActivitySplashScreenBinding
+import com.alossa.alossacapstone.ui.auth.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivitySplashScreenBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-//        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_splash_screen)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-//        binding.fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_splash_screen)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
---
+++ app/src/main/java/com/alossa/alossacapstone/ui/auth/ConfirmPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.alossa.alossacapstone.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alossa.alossacapstone.R
+
+class ConfirmPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_password)
+    }
+}
