e32c5f76c02f4a0a9bd642f65cd1847bab210364
Permana <permana4131@gmail.com>
2021-12-07 15:19:10 +0700

    Home Ui

---
+++ app/src/main/java/com/alossa/alossacapstone/ui/home/AddExpenditureActivity.kt
@@ -0,0 +1,12 @@
+package com.alossa.alossacapstone.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alossa.alossacapstone.R
+
+class AddExpenditureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_expenditure)
+    }
+}
